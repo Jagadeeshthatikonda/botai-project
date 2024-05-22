3b664187bd37b884b8e8e090116731d6ac8b7231
jagadeesh <jagadeeshnoddy12@example.com>
2024-05-22 16:49:11 +0530

    self review the questionand reply chart cards

--- src/components/QuestionAndReplyChatCards/QuestionAndReplyChatCards.jsx
+++ src/components/QuestionAndReplyChatCards/QuestionAndReplyChatCards.jsx
@@ -1,31 +1,16 @@
-import { useState } from "react"
-
-import * as Styled from './StyledComponents'
-import MyProfile from "../../assets/MyProfile.png"
-import HomeLogo from "../../assets/MyProfile.png"
-import Like from "../../assets/like.png"
-import Dislike from "../../assets/dislike.png"
-import ChatMessageCard from "../ChatMessageCard/ChatMessageCard"
-const QuestionAndReplyChatCards = ({ askedMessageWithResponse }) =>
-
-
-
-
-
-
+import * as Styled from "./StyledComponents";
+import ChatMessageCard from "../ChatMessageCard/ChatMessageCard";
+const QuestionAndReplyChatCards = ({ askedMessageWithResponse }) => (
   <Styled.MessagesListContainer>
-    {
-      ["question", "reply"].map(value => {
-
-        return <ChatMessageCard askedMessageWithResponse={askedMessageWithResponse} typeOfCard={value} />
-      })
-
-    }
-
-
+    {["question", "reply"].map((value) => {
+      return (
+        <ChatMessageCard
+          askedMessageWithResponse={askedMessageWithResponse}
+          typeOfCard={value}
+        />
+      );
+    })}
   </Styled.MessagesListContainer>
+);
 
-
-
-
-export default QuestionAndReplyChatCards
+export default QuestionAndReplyChatCards;
